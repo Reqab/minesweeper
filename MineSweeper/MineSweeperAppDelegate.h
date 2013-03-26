567128f1e6cc569eab5189749a687339139c8049
Adam Bennett <adambennett@ecs325-19.labs.encs>
2013-03-26 15:30:51 -0700

    Initial Commit

---
+++ MineSweeper/MineSweeperAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MineSweeperAppDelegate.h
+//  MineSweeper
+//
+//  Created by Adam Bennett on 3/26/13.
+//  Copyright (c) 2013 WSU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MineSweeperAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MineSweeper/MineSweeperAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MineSweeperAppDelegate.m
+//  MineSweeper
+//
+//  Created by Adam Bennett on 3/26/13.
+//  Copyright (c) 2013 WSU. All rights reserved.
+//
+
+#import "MineSweeperAppDelegate.h"
+
+@implementation MineSweeperAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
