79f06b6f6260bc48322efc86bacf14bf31304500
Adam Bennett <adambennett@ecs325-19.labs.encs>
2013-03-26 16:05:20 -0700

    Initial commit

---
+++ MineSweeper/MineSweeperController.h
@@ -0,0 +1,13 @@
+//
+//  MineSweeperController.h
+//  MineSweeper
+//
+//  Created by Adam Bennett on 3/26/13.
+//  Copyright (c) 2013 WSU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MineSweeperController : NSObject
+
+@end
---
+++ MineSweeper/MineSweeperController.m
@@ -0,0 +1,13 @@
+//
+//  MineSweeperController.m
+//  MineSweeper
+//
+//  Created by Adam Bennett on 3/26/13.
+//  Copyright (c) 2013 WSU. All rights reserved.
+//
+
+#import "MineSweeperController.h"
+
+@implementation MineSweeperController
+
+@end
